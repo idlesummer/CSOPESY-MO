333e9e165442f4eabdb5365cd1631e1897029713
Nash Luis Maramag <09louisthebob@gmail.com>
2025-07-04 13:25:51 +0800

    Add meta field to Instruction struct
    Introduces an unordered_map 'meta' field to the Instruction struct for storing additional metadata as key-value pairs.

--- src/core/instruction/Instruction.hpp
+++ src/core/instruction/Instruction.hpp
@@ -6,8 +6,10 @@ namespace csopesy {
   /** Represents a single process instruction. */
   struct Instruction {
     using list  = vector<str>;
+    using map = unordered_map<str, uint>;
     
     str opcode;
     list args;
+    map meta;
   };
 }
