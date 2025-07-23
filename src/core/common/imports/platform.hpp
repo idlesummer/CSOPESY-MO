c0ba8ea04f9f4e0cb7dd3dd5c0da2f30dc690f24
Nash Luis Maramag <09louisthebob@gmail.com>
2025-07-23 22:27:19 +0800

    Update platform.hpp

--- src/core/common/imports/platform.hpp
+++ src/core/common/imports/platform.hpp
@@ -1,15 +1,23 @@
 #pragma once
-#ifndef NOMINMAX
-  #define NOMINMAX
-#endif
-#include <windows.h> // Doesn't define min/max since NOMINMAX is set
-#include <algorithm> // Redefines std::min/std::max
 
+#if defined(_WIN32)
+  #ifndef NOMINMAX
+    #define NOMINMAX
+  #endif
+  #include <windows.h>
+  void enable_unicode() {
+    SetConsoleOutputCP(CP_UTF8);
+  }
+#else
+  #include <locale>
+  #include <iostream>
+  void enable_unicode() {
+    std::ios_base::sync_with_stdio(false);
+    std::setlocale(LC_ALL, "");
+    std::wcout.imbue(std::locale(""));
+  }
+#endif
+
+#include <algorithm>
 using std::min;
 using std::max;
-
-
-void enable_unicode() {
-  SetConsoleOutputCP(CP_UTF8);
-}
-
