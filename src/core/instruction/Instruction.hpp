c4b62c650236468ee3b97560f75da64e8b392841
Nash Luis Maramag <09louisthebob@gmail.com>
2025-07-23 21:58:47 +0800

    Update Instruction.hpp

--- src/core/instruction/Instruction.hpp
+++ src/core/instruction/Instruction.hpp
@@ -9,9 +9,9 @@ struct Instruction {
   str opcode;     ///< Operation name, e.g., `FOR`, `PRINT`, etc.
   vec<str> args;      ///< List of string arguments
 
-/**
- * Metadata: cached exit address for control-flow blocks, e.g., `FOR`, `IF`.
- * Set dynamically to allow skipping. A value of 0 means unset.
- */
+  /**
+   * Metadata: cached exit address for control-flow blocks, e.g., `FOR`, `IF`.
+   * Set dynamically to allow skipping. A value of 0 means unset.
+   */
   mutable uint exit = 0;
 };
