4a71d79ca322ca32fcf46b9246c08f41f4ab8fc3
Nash Luis Maramag <09louisthebob@gmail.com>
2025-07-25 04:45:10 +0800

    banner change

--- src/core/common/constants/banner.hpp
+++ src/core/common/constants/banner.hpp
@@ -2,15 +2,12 @@
 #include <string>
 
 inline const std::string BANNER = R"(
-         .dP                                                                        Yb    
-       .dP    .ooooo.   .oooo.o  .ooooo.  oo.ooooo.   .ooooo.   .oooo.o oooo    ooo  `Yb  
-      dP     d88' `"Y8 d88(  "8 d88' `88b  888' `88b d88' `88b d88(  "8  `88.  .8'     `Yb
-      Yb     888       `"Y88b.  888   888  888   888 888ooo888 `"Y88b.    `88..8'      .dP
-       `Yb   888   .o8 o.  )88b 888   888  888   888 888    .o o.  )88b    `888'     .dP  
-         `Yb `Y8bod8P' 8""888P' `Y8bod8P'  888bod8P' `Y8bod8P' 8""888P'     .8'     dP    
-                                           888                          .o..P'            
-                                          o888o                         `Y8P'             
+  ██╗  ██████╗ ███████╗  ██████╗  ██████╗  ███████╗ ███████╗ ██╗   ██╗ ██╗  
+ ██╔╝ ██╔════╝ ██╔════╝ ██╔═══██╗ ██╔══██╗ ██╔════╝ ██╔════╝ ╚██╗ ██╔╝ ╚██╗ 
+██╔╝  ██║      ███████╗ ██║   ██║ ██████╔╝ █████╗   ███████╗  ╚████╔╝   ╚██╗
+╚██╗  ██║      ╚════██║ ██║   ██║ ██╔═══╝  ██╔══╝   ╚════██║   ╚██╔╝    ██╔╝
+ ╚██╗ ╚██████╗ ███████║ ╚██████╔╝ ██║      ███████╗ ███████║    ██║    ██╔╝ 
+  ╚═╝  ╚═════╝ ╚══════╝  ╚═════╝  ╚═╝      ╚══════╝ ╚══════╝    ╚═╝    ╚═╝
 
-                          Like a Real OS, But Not Written in Assembly!        
-                              Dicayanan|Maramag|Maunahan|Villaver                         
+                    Dicayanan|Maramag|Maunahan|Villaver                         
 )";
